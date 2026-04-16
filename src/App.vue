95c61b915f65fc7cdbf70dccfa75c6eb7c80c24f
Alumno Ifab <alumno@gmail.com>
2026-04-16 18:27:14 +0200

    plopher: Formulario de contacto finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import 'vue-sonner/style.css'
+import { Toaster as SonnerToaster} from '@/components/ui/sonner'
 
 </script>
 
 <template>
   <main>
     <RouterView />
+
+    <SonnerToaster />
   </main>
 </template>
--- src/paginae/batman/Batman.vue
+++ src/paginae/batman/Batman.vue
@@ -1,10 +1,40 @@
 <script lang="ts" setup>
-
-
-import Carrusimaginum from '@/components/carrusimaginum.vue';
+import Carrusimaginum from '@/components/Carrusimaginum.vue';
 import NavigatorPrimarius from '@/components/NavigatorPrimarius.vue';
 import { useMouseMotion } from '@/composables/useMouseMotion';
 import {scrolltoSection} from '@/utils/scrollToSection'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+
+import {ref} from 'vue'
+import type { DateValue } from 'reka-ui';
+import { Instagram, Linkedin, Loader2, LucideBadgeCheck, Mail, MapPin, MessageCircle, Phone, Twitter } from 'lucide-vue-next';
+import { toast } from 'vue-sonner';
+
+  const dies = ref<DateValue>()
+
+  const estLoading = ref<boolean>(false)
+
+  const nomen = ref<string>("")
+  const cognomen = ref<string>("")
+  const missio = ref<string>("")
+
 
   const { cumMouseLeave, cumMouseMove, mousePositione } = useMouseMotion()
 
@@ -33,6 +63,41 @@ import {scrolltoSection} from '@/utils/scrollToSection'
     },
 
   ]
+
+  const mittereSubmit = async() => {
+    estLoading.value = true
+
+      toast(`Solicitud enviada correctamente
+        - Nombre: ${nomen.value} ${cognomen.value}
+        - Misión: ${missio.value}
+        - Fecha: ${dies.value 
+            ? `${dies.value.day}/${dies.value.month}/${dies.value.year}` 
+            : 'No especificada'}`, {
+          duration: 4000,
+          position: 'top-right',
+          icon: LucideBadgeCheck,
+          style: {
+            background: '#201c3b',
+            color: '#fff',
+            whiteSpace: 'pre-wrap',
+          }
+        })
+
+    await new Promise (resolve => setTimeout(resolve,2000))
+    //lógica de Backend iría aquí//
+    console.log('Formulario enviado:', { 
+      nomen: nomen.value,
+      cognomen: cognomen.value,
+      missio: missio.value,
+      dies: dies.value,
+    })
+
+    estLoading.value = false
+    nomen.value = ""
+    cognomen.value = ""
+    missio.value = ""
+    dies.value = undefined
+  }
 
 </script>
 <template>
@@ -102,6 +167,151 @@ class="titulus-img"
   />
 
  </section>
+
+ <section id="contactus" class="w-full py-12 bg-gray-100">
+
+  <div class="container mx-auto max-w-5xl px-4">
+
+    <h2 class="text-3xl font-bold text-gray-900 mb-8 text-center">Contacta con Batman</h2>
+          
+    <div class="flex flex-col lg:flex-row gap-8 items-center">
+
+            <!-- Mapa de Google Maps -->
+      <div class="hidden lg:block w-full lg:w-1/2 rounded-lg overflow-hidden shadow-lg">
+        <div class="aspect-square">
+          <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.336782676313!2d-0.34719482319661027!3d39.48430927160479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd60488258df2ba7%3A0xe3abb39ef4edbfba!2sBellas%20Artes%20Polit%C3%A9cnica!5e0!3m2!1ses!2ses!4v1776350782948!5m2!1ses!2ses" 
+          width="600" height="450" 
+          :style="{border:0}" allowfullscreen
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+        </div>
+      </div>
+
+            <!-- Formulario -->
+      <div class="w-full max-w-md mx-auto lg:max-w-none lg:w-1/2 ">
+        <form  class="space-y-6 bg-white p-8 rounded-lg shadow-lg aspect-square"
+        @submit.prevent="mittereSubmit"
+        >
+          <div class="space-y-2">
+                  
+            <Label for="nomen">Nombre</Label>
+            <Input id="nomen" required v-model="nomen" />
+
+          </div>
+                
+          <div class="space-y-2">
+              
+            <Label for="cognomen">Apellidos</Label>
+            <Input id="cognomen" required  v-model="cognomen"/>
+
+          </div>
+
+          <div class="space-y-2">
+
+            <Select required v-model="missio">
+              <SelectTrigger>
+                <SelectValue placeholder="Seleccionar una misión" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectItem value="rescatare">
+                  Rescate de rehenes
+                </SelectItem>
+                <SelectItem value="investigare">
+                  Investigación criminal
+                </SelectItem>
+                <SelectItem value="persequi">
+                  Persecución de villanos
+                </SelectItem>
+                <SelectItem value="defendere">
+                  Defensa de Gotam
+                </SelectItem>
+              </SelectContent>
+            </Select>
+
+          </div>
+
+          <div class="space-y-2">
+
+            <Label>Fecha de la misión</Label>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline">
+                  <span v-if="dies">
+                    {{ dies.day }}/{{ dies.month }}/{{ dies.year }}
+                  </span>
+                  <span v-else>
+                    Selecciona una fecha
+                  </span>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Calendar v-model="dies" />
+              </PopoverContent>
+            </Popover>
+          </div>
+
+            <Button type="submit"
+            class="w-full bg-[rgb(106,90,205)] hover:bg-[rgb(88,75,171)] text-white text-md mt-4"
+            :disabled="estLoading" >
+            
+            <Loader2 v-if="estLoading" class="mr-2 w-4 animate-spin" />
+              <span v-if="estLoading">Enviando...</span>
+              <span v-else>Enviar solicitud</span>
+            </Button>
+              
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<footer class="w-full bg-gray-900 text-gray-300 py-12">
+   <div class="flex flex-col md:flex-row md:justify-around gap-8 max-w-3xl mx-auto">
+
+    <div class="space-y-4 text-center md:text-left">
+
+      <h3 class="text-xl font-bold text-white">Información de Contacto</h3>
+            
+        <div class="space-y-2">
+          <p class="flex items-center gap-2 justify-center md:justify-start">
+
+            <Phone class="w-5 h-5"/>
+            +1 (555) 123-4567
+
+          </p>
+          <p class="flex items-center gap-2 justify-center md:justify-start">
+
+            <Mail class="w-5 h-5"/>
+            batman@wayneenterprises.com
+
+          </p>
+          <p class="flex items-center gap-2 justify-center md:justify-start">
+
+            <MapPin class="w-5 h-5"/>
+            Wayne Manor, Gotham City
+
+          </p>
+        </div>
+      </div>
+
+      <!-- Redes sociales -->
+      <div class="space-y-4 text-center md:text-left text-gray-400">
+        <h3 class="text-xl font-bold text-white">Síguenos</h3>
+        <div class="flex gap-8 justify-center">
+        
+          <Twitter class="w-10 h-10 hover:text-white" />
+          <Instagram class="w-10 h-10 hover:text-white" />
+          <Linkedin class="w-10 h-10 hover:text-white" />
+          <MessageCircle class="w-10 h-10 hover:text-white" />
+              
+        </div>
+    </div>
+  </div>
+</footer>
+
 
 </div>
 

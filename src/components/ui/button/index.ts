d5f12753f2f3d06dcf4c37cb54d13b9206ca9300
Alumno Ifab <alumno@gmail.com>
2026-04-21 17:23:40 +0200

    plopher: Página de inicio de Simpsons y componentes son SVG

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -19,6 +19,7 @@ export const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        tertiarius: "bg-white text-base border border-black hover:bg-[#ee3123] focus:bg-[ee3123] rounded-none"
       },
       size: {
         "default": "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -27,6 +28,7 @@ export const buttonVariants = cva(
         "icon": "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
+        "elongatis": "h-7.5 py-0.5 px-5"
       },
     },
     defaultVariants: {
--- src/paginae/simpsons/Primus.vue
+++ src/paginae/simpsons/Primus.vue
@@ -1,6 +1,40 @@
+<script lang="ts" setup>
+import LogoSimpsons from '@/components/LogoSimpsons.vue';
+import { Button } from '@/components/ui/button';
+
+
+</script>
+
 
 <template>
   
-<h1 class="text-6xl m-8 text-red-500 bg-amber-300 w-fit px-12 py-5 rounded-2xl shadow-xl hover:bg-red-500 hover:text-white">Simpsons Page</h1>
+<div class="bg-[#ffde00] min-h-screen text-center p-4 flex flex-col items-center justify-center">
+	
+    <LogoSimpsons estilus="w-[24rem]" />
+    
+    <h1 class="text-xl my-1 font-medium">
+    	Discover some of the most amazing Quotes from The Simpsons
+	</h1>
+	<div>
+    	<strong>This is an unofficial app (even though it looks so cool!)</strong>
+	</div>
+	<div className="mt-8 flex gap-5">
+		<Button size="elongatis" variant="tertiarius">
+      		<RouterLink to="/">Exit</RouterLink>
+    	</Button>
+    	<Button size="elongatis" variant="tertiarius">
+      		<RouterLink to="">Enter</RouterLink>
+    	</Button>
+	</div>
+
+    <div class="flex gap-12">
+        <img src="/imagines/simpsons/bart.png" alt="" class="object-contain hidden md:block" width="200px">
+        <img src="/imagines/simpsons/homer.png" alt="" class="object-contain hidden md:block mt-20" width="195px">
+        <img src="/imagines/simpsons/moe-s.png" alt="" lass="object-contain mt-12 md:mt-0" width="240px">
+
+    </div>
+
+
+</div>
 
 </template>
